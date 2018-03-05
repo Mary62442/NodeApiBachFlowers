3c3fa4dbef25dc9c6271c38ca38eb9af9f117688
Maria Burlando <maria.bu62442@gmail.com>
2018-03-05 16:03:02 +0100

    Added APIs for 4 step token verification case study in diegomary website

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ let path = require('path');
 let fs = require('fs');
 let bodyParser = require('body-parser');
 let auth = require('basic-auth');
+let secret = require('./secret.js');
+let jwt = require('jsonwebtoken');
 
 let allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With, Custom-Auth-Step1, Custom-Auth-Step2, Custom-Auth-Step3, Custom-Auth-Step4');
     // intercept OPTIONS method
 
     if ('OPTIONS' == req.method) {
@@ -56,8 +58,10 @@ app.post('/', (req, res, next) => {
 
 app.get('/help', (req, res, next) => {
    
-    res.send('Help Page');   
+    res.send('Help Page update today 5/3/2018');   
 });
+
+
 
 
 app.get('/flowerstatic', (req, res, next) => {   
@@ -144,8 +148,139 @@ app.post('/updateflowernotes', (req, res, next) => {
 });
 
 
+app.post('/firststepsecurity', (req,res) => { 
 
-let serverUnsecure = http.createServer(app).listen(process.env.PORT || 3000, function () {
+    let firstPassword = req.get('custom-auth-step1');   
+    let username = req.body.username;
+    let memorableA = req.body.memorableA; 
+
+    let user = secret.secretData.users.find( user => user.name === username );
+        if (typeof user === 'undefined') {
+            res.json({success: false, message:'Failure. Invalid credentials'}); 
+        };
+    if (memorableA === user.memorableA) {
+        if (firstPassword === user.firstPassword) {
+            let token = jwt.sign(secret.secretData.signInEntityCaptain, secret.secretData.tokenSignInSecretCaptain, {
+                expiresIn: "2 days"
+              });
+              res.json({
+                success: true,
+                message: 'First JWT token created and signed by Captain America!',
+                username:user.name,
+                token: token,
+                secondRoute: secret.secretData.secondRoute
+              });
+        }
+        else res.json({success: false, message:'Failure. Invalid credentials'}); 
+    }      
+    else {
+        res.json({success: false, message:'Failure. Invalid credentials'});
+    }    
+});
+
+app.post('/tfrytiruytrrgr56767j98', (req,res) => {
+    let secondPassword = req.get('custom-auth-step2');   
+    let token1 = req.body.token1;
+    let username = req.body.username;
+
+    let user = secret.secretData.users.find( user => user.name === username );
+        if (typeof user === 'undefined') {
+            res.json({success: false, message:'Failure. Invalid credentials'}); 
+        };
+
+    if (secondPassword === user.secondPassword) {        
+        if (token1) {
+            // verifies secret and checks exp
+            jwt.verify(token1, secret.secretData.tokenSignInSecretCaptain, (err, decoded) => {
+                if (err) {
+                return res.json({ success: false, message: 'Failure. Invalid credentials' });
+                }                 
+                let token2 = jwt.sign(secret.secretData.signInEntityGeneral, secret.secretData.tokenSignInSecretGeneral, {
+                    expiresIn: "2 days"
+                    });
+                res.json({
+                    success: true,
+                    message: 'Second JWT token created and signed by General Shang!',
+                    token: token2,
+                    username: user.name,
+                    thirdRoute: secret.secretData.thirdRoute
+                });                
+            });        
+        }
+    }
+    else {
+        res.json({success: false, message:'Failure. Invalid credentials'});
+    }
+});
+
+
+app.post('/dhaetyhaetgdhbfgbn5674ser', (req,res) => {
+
+    let thirdPassword = req.get('custom-auth-step3');   
+    let token2 = req.body.token2;
+    let username = req.body.username;
+
+    let user = secret.secretData.users.find( user => user.name === username );
+        if (typeof user === 'undefined') {
+            res.json({success: false, message:'Failure. Invalid credentials'}); 
+        };
+
+    if (thirdPassword === user.thirdPassword) {        
+        if (token2) {
+            // verifies secret and checks exp
+            jwt.verify(token2, secret.secretData.tokenSignInSecretGeneral, (err, decoded) => {
+                if (err) {
+                    return res.json({ success: false, message: 'Failure. Invalid credentials' });
+                }                 
+                let token3 = jwt.sign(secret.secretData.signInEntityCommander, secret.secretData.tokenSignInSecretCommander, {
+                    expiresIn: "2 days"
+                    });
+                res.json({
+                    success: true,
+                    username: user.name,
+                    message: 'Third JWT token created and signed by President Abraham Lincoln!',
+                    token: token3,
+                    fourthRoute: secret.secretData.fourthRoute
+                });                
+            });        
+        }
+    }
+    else {
+        res.json({success: false, message:'Failure. Invalid credentials'});
+    } 
+});
+
+app.post('/sdjogfnwo0aq23aojfnapw', (req,res) => {
+    let fourthPassword = req.get('custom-auth-step4');   
+    let token3 = req.body.token3;
+    let username = req.body.username;
+
+    let user = secret.secretData.users.find( user => user.name === username );
+        if (typeof user === 'undefined') {
+            res.json({success: false, message:'Failure. Invalid credentials'}); 
+        };
+
+    if (fourthPassword === user.fourthPassword) {        
+        if (token3) {
+            // verifies secret and checks exp
+            jwt.verify(token3, secret.secretData.tokenSignInSecretCommander, (err, decoded) => {
+                if (err) {
+                return res.json({ success: false, message: 'Failure. Invalid credentials' });
+                }                                     
+                res.json({
+                    success: true,
+                    message: 'Success! You have accessed the most secret data!',
+                    superSecretData:'Five busy honey bees were resting in the sun. The first one said, "Let us have some fun." The second one said, "Where shall it be?" The third one said, "In the honey tree." The fourth one said, "Let us make some honey sweet." The fifth one said, "With pollen on our feet." The five little busy bees sang their buzzing tune, As they worked in the beehive all that afternoon. Bzzzzzz! Bzzzzzz! Bzzzzzz! Bzzzzzz! Bzzzzzz!'
+                });                
+            });        
+        }
+    }
+    else {
+        res.json({success: false, message:'Failure. Invalid credentials'});
+    }
+});
+
+let serverUnsecure = http.createServer(app).listen(process.env.PORT || 3200, function () {
     console.log('Server express running at. ' + serverUnsecure.address().address + ':' + serverUnsecure.address().port + ' ');
 });
 
---
+++ secret.js
@@ -0,0 +1,60 @@
+
+
+
+exports.secretData = {
+    signInEntityCaptain : {
+        name:'Captain America', 
+        organization:'Avengers', 
+        website:'avengers.com',
+        contactEmail:'captainamerica@gmail.com',
+        role:['Captain']
+    },
+    signInEntityGeneral : {
+        name:'General Shang', 
+        organization:'Mulan', 
+        website:'disney.com',
+        contactEmail:'generalshang@gmail.com',
+        role:['Captain','General']
+    },
+    signInEntityCommander : {
+        name:'The President of the United States', 
+        organization:'USA', 
+        website:'usa.com',
+        contactEmail:'abrahamlincoln@gmail.com',
+        role:['Captain','General','Commander in Chief']
+    },
+    tokenSignInSecretCaptain : '4g6je5fs',
+    tokenSignInSecretGeneral : 'j6e8gd44g',
+    tokenSignInSecretCommander : '2d4g79ks',
+
+    
+    secondRoute:'tfrytiruytrrgr56767j98',
+    thirdRoute:'dhaetyhaetgdhbfgbn5674ser',
+    fourthRoute:'sdjogfnwo0aq23aojfnapw',
+
+    users: [
+        {
+            name:'DM88',
+            memorableQ:'Favourite season?',
+            memorableA:'autumn',
+            firstPassword: 'secret',
+            secondPassword : 'hidden',
+            thirdPassword : 'concealed',
+            fourthPassword : 'inaccessible'
+            
+        },
+        {
+            name:'Rapunzel',
+            memorableQ:'Favourite season?',
+            memorableA:'summer',
+            firstPassword: 'secret',
+            secondPassword : 'hidden',
+            thirdPassword : 'concealed',
+            fourthPassword : 'inaccessible'
+            
+        }
+    ]
+
+    
+}
+
